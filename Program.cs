df3ce7b5cbaf264ada2f979b4ff5521b89fcb2dc
aidanlmccloy <43994783+aidanlmccloy@users.noreply.github.com>
2018-11-16 22:02:45 -0500

    Update 2.0

--- Program.cs
+++ Program.cs
@@ -10,22 +10,25 @@ namespace InterfacesDemo
             Console.WriteLine($"YoPapa has {yopapa.Health}");
             Orc grendal = new Orc("Grendal", 85);
             Console.WriteLine($"Grendal has {grendal.Health}");
+            int Round = 0;
             while ((grendal.Health > 0) && (yopapa.Health > 0))
             {
+                Round++;
                 double Damage = grendal.Attack(grendal.Armour);
                 grendal.TakeDamage(Damage);
-                Console.WriteLine($"Yopapa's health: {grendal.Health} Amrour:{grendal.Armour}");
+                Console.WriteLine($"Round: {Round}"); 
+                Console.WriteLine($"Grendal's health: {grendal.Health} Amrour:{grendal.Armour}");
                 Damage = yopapa.Attack(yopapa.Armour);
                 yopapa.TakeDamage(Damage);
-                Console.WriteLine($"Yopapa's health: {yopapa.Health} Amrour:{yopapa.Armour}");
+                Console.WriteLine($"Yopapa's health: {yopapa.Health} Amrour:{yopapa.Armour}" + "\n");
             }
             if (grendal.Health > 0)
             {
-                Console.WriteLine($"Grendal Proves Victorious");
+                Console.WriteLine($"Press F to pay respects to Yopapa");
             }
             else
             {
-                Console.WriteLine($"YoPapa Proves Victorious");
+                Console.WriteLine($"YoPapa Proves Victorious!");
             }
 
         }
